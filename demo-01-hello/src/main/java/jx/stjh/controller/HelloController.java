9225594bcc6f2c6c17942887788c36742e722ccc
mowuqinakun <m17770879759@163.com>
2020-03-15 22:30:32 +0800

    提交

--- demo-01-hello/src/main/java/jx/stjh/controller/HelloController.java
+++ demo-01-hello/src/main/java/jx/stjh/controller/HelloController.java
@@ -1,6 +1,8 @@
 package jx.stjh.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,11 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @Auther: Zlk
  * @Date: 2020/3/12 00:15
  */
-
+@Api(tags = "swagger例子")  //swagger类
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
+    //@RequestMapping("/hello")
+    @ApiOperation("swagger 方法!")  //swagger方法
+    //@ApiIgnore   //屏蔽swagger接口说明
+    @GetMapping("/hello")
     public String hello(){
         return "hello springboot！！";
     }
--- demo-01-hello/src/main/java/jx/stjh/demo01hello/Demo01HelloApplication.java
+++ demo-01-hello/src/main/java/jx/stjh/demo01hello/Demo01HelloApplication.java
@@ -3,19 +3,20 @@ package jx.stjh.demo01hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
-/*
+/**
 1、热部署；
 2、springboot利用idea创建
 3、github如果创建分类
 4、Springboot入门
+
+ * @author zlk
  */
 @SpringBootApplication
 @ComponentScan(basePackages={"jx.stjh.*"})
 public class Demo01HelloApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(Demo01HelloApplication.class, args);
     }
+
 
 }
