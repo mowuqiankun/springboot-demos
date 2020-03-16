daebe767068fa7f7eb2103789d45a279b4cb5dda
zlk <m17770879759@163.com>
2020-03-16 16:46:46 +0800

    大更新

---
+++ demo-02-mybatis/README.md
@@ -0,0 +1,8 @@
+1、springboot-web 搭建；
+2、热部署；
+3、swagger
+   (访问：localhost:8080/swagger-ui.html)
+5、mybatis;
+6、集成Druid数据源(为监控而生的DB池，阿里开源的JDBC应用组件，主要包含三部分)
+    访问：localhost:8080/druid/index.html
+7、log4j集成
---
+++ demo-02-mybatis/src/test/java/jx/sjth/demo02mybatis/Demo02MybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package jx.sjth.demo02mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo02MybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
